9140835c21dffdc7c99640d018c2dccd2d856051
Kartik Budania <kartik.budania.02@gmail.com>
2023-04-24 12:38:46 +0530

    sidebar functionality added

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -2,19 +2,24 @@ import React from 'react'
 import './sidebar.css'
 import profile from './profileimage.jpg'
 import SidebarButton from './sidebarButton'
+import {MdFavorite} from 'react-icons/md'
+import {FaGripfire, FaPlay} from 'react-icons/fa'
+import {FaSignOutAlt} from 'react-icons/fa'
+import {IoLibrary} from 'react-icons/io5'
+import {MdSpaceDashboard} from 'react-icons/md'
 
 export default function Sidebar() {
   return (
     <div className='sidebar-container'>
       <img src={profile} className='profile-img' alt='profile picture' />
       <div>
-        <SidebarButton  />
-        <SidebarButton  />
-        <SidebarButton  />
-        <SidebarButton  />
-        <SidebarButton  />
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+        <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />} />
+        <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
-        <SidebarButton  />
+        <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
     </div>
   )
 }
