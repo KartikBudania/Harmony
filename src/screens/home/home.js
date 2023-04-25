d19b1815c7cc04f8e309b8c661747d77d45b45ef
Kartik Budania <kartik.budania.02@gmail.com>
2023-04-25 20:21:22 +0530

    changes made in home js for login window but not working right

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Library from '../library/library'
 import Feed from '../feed/feed'
@@ -7,9 +7,30 @@ import Player from '../player/player'
 import Favorites from '../favorites/favorites'
 import './home.css'
 import Sidebar from '../../components/sidebar/sidebar'
+import Login from '../auth/login'
 
 export default function Home() {
-  return (
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    const hash = window.localStorage.hash;
+    // window.location.hash = "";
+    if(!token && hash){
+      const _token = hash.split("&")[0].split("=")[1];
+      // console.log(_token)
+      window.localStorage.setItem('token', _token);
+      setToken(_token);
+    }else{
+      setToken(token)
+    }
+  }, [])
+
+  console.log(token)
+
+  return !token ? (
+    <Login />
+  ) : (
     <Router>
       <div className='main-body'>
         <Sidebar />
