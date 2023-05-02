885437e234625f6e184d8d03be310cfabbe275c9
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-02 23:03:17 +0530

    added playlists in library

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import APIKit from "../../spotify";
+import  './library.css';
 
 export default function Library() {
+  const [playlists, setPlaylists] = useState(null);
+
+  useEffect(() => {
+    APIKit.get("me/playlists").then((response) => {
+      setPlaylists(response.data.items);
+      console.log(response.data.items);
+    });
+  }, []);
+
   return (
-    <div className='screen-container'>Library</div>
-  )
+    <div className="screen-container">
+      <div className="library-body">
+        {playlists?.map((playlist) => (
+          <div className="playlist-card">{playlist.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
