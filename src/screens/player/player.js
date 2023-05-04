9a46f60145e8f0d4e1e74beac40d4a2d18c708dd
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-05 01:35:50 +0530

    setup songCard in player

--- src/screens/player/player.js
+++ src/screens/player/player.js
@@ -1,20 +1,33 @@
-import React, { useEffect } from 'react'
-import './player.css'
-import { useLocation } from 'react-router-dom'
-import apiClient from '../../spotify'
+import React, { useEffect, useState } from "react";
+import "./player.css";
+import { useLocation } from "react-router-dom";
+import apiClient from "../../spotify";
+import SongCard from "../../components/songCard/songCard"
+import Queue from "../../components/queue/queue"
 
 export default function Player() {
-  const location = useLocation()
+  const location = useLocation();
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() =>{
-    apiClient.get(`playlists/${location.state?.id}/tracks`)
-    .then(res => console.log(res.data))
-  }, [location.state])
+  useEffect(() => {
+    if (location.state) {
+      apiClient.get(`playlists/${location.state?.id}/tracks`)
+      .then((res) => {
+        setTracks(res.data.items);
+        setCurrentTrack(res.data.items[0].track);
+      });
+    }
+  }, [location.state]);
 
   return (
-    <div className='screen-container'>
-      <div className='left-player-body'></div>
-      <div className='right-player-body'></div>
+    <div className="screen-container flex">
+      <div className="left-player-body"></div>
+      <div className="right-player-body">
+        <SongCard album={currentTrack.album} />
+        <Queue />
+      </div>
     </div>
-  )
+  );
 }
