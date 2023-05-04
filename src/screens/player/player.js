37255143828fd8da459c6d1f84ea4d5762373925
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-05 00:03:07 +0530

    fetch api for the playlist

--- src/screens/player/player.js
+++ src/screens/player/player.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './player.css'
+import { useLocation } from 'react-router-dom'
+import apiClient from '../../spotify'
 
 export default function Player() {
+  const location = useLocation()
+
+  useEffect(() =>{
+    apiClient.get(`playlists/${location.state?.id}/tracks`)
+    .then(res => console.log(res.data))
+  }, [location.state])
+
   return (
-    <div className='screen-container'>Player</div>
+    <div className='screen-container'>
+      <div className='left-player-body'></div>
+      <div className='right-player-body'></div>
+    </div>
   )
 }
