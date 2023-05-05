3a1c206887fd6aff6b4db9e02227f560f6f54680
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-06 00:21:02 +0530

    setup album info

--- src/components/songCard/albumInfo.js
+++ src/components/songCard/albumInfo.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import './albumInfo.css'
 
-export default function AlbumInfo() {
+export default function AlbumInfo({album}) {
+  // console.log(album);
+
+  const artists = []
+
+  album?.artists?.forEach(element => {
+    artists.push(element.name)
+  });
+
   return (
-    <div>AlbumInfo</div>
+    <div className='albumInfo-card'>
+      <div className='albumName-container'>
+        <p>{album?.name + " - " + artists?.join(", ")}</p>
+      </div>
+      <div className='album-info'>
+        <p>{`${album?.name} is a ${album?.album_type} with ${album?.total_tracks} track(s)`}</p>
+      </div>
+      <div className='album-release'>
+        <p>Release Date: {album?.release_date}</p>
+      </div>
+    </div>
   )
 }
