05013ab9ec1ed7154441a292c5db0fac5e266298
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-07 20:03:58 +0530

    added items and duration of songs in the queue list

--- src/components/queue/queue.js
+++ src/components/queue/queue.js
@@ -2,11 +2,22 @@ import React from 'react'
 import './queue.css'
 
 export default function Queue({tracks, setCurrentIndex}) {
+
+  // console.log(tracks);
   return (
     <div className='queue-container flex'>
       <div className='queue flex'>
         <p className='upNext'>Up Next</p>
-        <div className='queue-list'></div>
+        <div className='queue-list'>
+          {
+            tracks?.map((track) => (
+              <div className='queue-item flex'>
+                <p className='track-name'>{track?.track?.name}</p>
+                <p>{Math.floor(((track?.track?.duration_ms)/1000/60)<<0)}:{(Math.floor(((track?.track?.duration_ms)/1000)%60)) < 10 ? `0${(Math.floor(((track?.track?.duration_ms)/1000)%60))}`: `${(Math.floor(((track?.track?.duration_ms)/1000)%60))}`}</p>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
