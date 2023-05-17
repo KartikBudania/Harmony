db34a69e669b97feff4143ce0c98256a952c413f
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-17 19:29:44 +0530

    added current song details and css in the audio player

--- src/components/audioplayer/audioPlayer.js
+++ src/components/audioplayer/audioPlayer.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './audioPlayer.css'
 import ProgressCircle from './progressCircle'
+import Controls from './controls';
+import WaveAnimation from './waveAnimation';
 
 export default function AudioPlayer({currentTrack}) {
+  const artists =[];
+  currentTrack?.album?.artists.forEach((artist) => {
+    artists.push(artist.name);
+  });
   return (
     <div className='player-body flex'>
         <div className='player-left-body'>
@@ -14,7 +20,24 @@ export default function AudioPlayer({currentTrack}) {
                 color="#C96850"
             />
         </div>
-        <div className='player-right-body'></div>
+        <div className='player-right-body flex'>
+          <p className='song-title'>{currentTrack?.name}</p>
+          <p className='song-artist'>{artists.join(' | ')}</p>
+          <div className='player-right-bottom flex'>
+          <div className='song-duration flex'>
+            <p className='duration'>0:01</p>
+            <WaveAnimation />
+            <p className='duration'>0:30</p>
+          </div>
+          <Controls 
+            // isPlaying={isPlaying}
+            // setIsPlaying={setIsPlaying}
+            // handleNext={handleNext}
+            // handlePrev={handlePrev}
+            // total={total}
+          />
+          </div>
+        </div>
     </div>
   )
 }
