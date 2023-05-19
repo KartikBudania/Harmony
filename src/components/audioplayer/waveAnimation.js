5947710e71668c3818f27b89388918479fde30cb
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-19 22:43:12 +0530

    waveAnimation.js

--- src/components/audioplayer/waveAnimation.js
+++ src/components/audioplayer/waveAnimation.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import './waveAnimation.css'
+import React from "react";
+import "./waveAnimation.css";
 
-export default function WaveAnimation({isPlaying}) {
-
-  const waveClass = isPlaying ? 'box active' : 'box';
+export default function WaveAnimation({ isPlaying }) {
+  const waveClass = isPlaying ? "box active" : "box";
 
   return (
-    <div className='box-container flex'>
+    <div className="box-container flex">
       <div className={`${waveClass} box1`}></div>
       <div className={`${waveClass} box2`}></div>
       <div className={`${waveClass} box3`}></div>
@@ -21,5 +20,5 @@ export default function WaveAnimation({isPlaying}) {
       <div className={`${waveClass} box6`}></div>
       <div className={`${waveClass} box7`}></div>
     </div>
-  )
+  );
 }
