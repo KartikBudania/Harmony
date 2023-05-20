6528a5381e1eab9ec5a71e5cd11104aeb4da8768
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-20 22:26:17 +0530

    sidebar.js

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,33 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import './sidebar.css'
-import SidebarButton from './sidebarButton'
-import {MdFavorite} from 'react-icons/md'
-import {FaGripfire, FaPlay} from 'react-icons/fa'
-import {FaSignOutAlt} from 'react-icons/fa'
-import {IoLibrary} from 'react-icons/io5'
-import {MdSpaceDashboard} from 'react-icons/md'
-import apiClient from '../../spotify'
+import React, { useEffect, useState } from "react";
+import "./sidebar.css";
+import SidebarButton from "./sidebarButton";
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
+import apiClient from "../../spotify";
 
 export default function Sidebar() {
-  const [image, setImage] = useState('https://www.whoa.in/download/beautiful-girl-image-for-profile-picture-stock-photos')
+  const [image, setImage] = useState(
+    "https://www.whoa.in/download/beautiful-girl-image-for-profile-picture-stock-photos"
+  );
 
   useEffect(() => {
-    apiClient.get('me').then( response => {
+    apiClient.get("me").then((response) => {
       setImage(response.data.images[0].url);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
-    <div className='sidebar-container'>
-      <img src={image} className='profile-img' alt='profile picture' />
+    <div className="sidebar-container">
+      <img src={image} className="profile-img" alt="profile picture" />
       <div>
         <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
         <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
         <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
-        <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />} />
+        <SidebarButton
+          title="Favorites"
+          to="/favorites"
+          icon={<MdFavorite />}
+        />
         <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
-        <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
     </div>
-  )
+  );
 }
