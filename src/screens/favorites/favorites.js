da67f5db49ccff05724bf9e912d8f3eb61734825
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-24 22:08:43 +0530

    favorites.js

--- src/screens/favorites/favorites.js
+++ src/screens/favorites/favorites.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Favorites() {
-  return (
-    <div className='screen-container'>Favorites</div>
-  )
+  return <div className="screen-container">Favorites</div>;
 }
