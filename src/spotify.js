fdf1b29d0acb562844549c5aa783cbb4c797f8b0
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-26 12:00:27 +0530

    trying again for redirectUri

--- src/spotify.js
+++ src/spotify.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const authEndpoint = "https://accounts.spotify.com/authorize?"
 const clientId = "ba2becbc406c4c4fa990dc33bdbdeb7b"
-const redirectUri = "https://kartikbudania.github.io/Harmony/"
-// const redirectUri = "http://localhost:3000"
+// const redirectUri = "https://kartikbudania.github.io/Harmony/"
+const redirectUri = "http://localhost:3000"
 const scopes = ["user-library-read", "playlist-read-private"]
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
