e9d47545452e3ed7185d28a2d4618601b385ab5e
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-27 14:16:39 +0530

    added functionality of SignOut button

---
+++ src/components/sidebar/signOut.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { IconContext } from "react-icons";
+import { FaSignOutAlt } from "react-icons/fa";
+import "./signOut.css";
+
+export default function SignOut() {
+  function signOut(){
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
+
+  return (
+    <div className="signOut-body" onClick={signOut}>
+      <IconContext.Provider value={{ size: "24px", className: "signOut-icon" }}>
+        <FaSignOutAlt />
+        <p className="signOut-title">Sign Out</p>
+      </IconContext.Provider>
+    </div>
+  );
+}
