60e834ff21b4a0a5fc78f4cc8654f81f9cf85a5d
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-27 15:04:13 +0530

    redirect Uri changed

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const authEndpoint = "https://accounts.spotify.com/authorize?"
 const clientId = "ba2becbc406c4c4fa990dc33bdbdeb7b"
-const redirectUri = "https://kartikbudania.github.io/Harmony"
+const redirectUri = "https://kartikbudania.github.io/Harmony/"
 // const redirectUri = "http://localhost:3000"
 const scopes = ["user-library-read", "playlist-read-private"]
 
