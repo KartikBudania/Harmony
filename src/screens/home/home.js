15328358dbbd197febaae852e50291cfda908091
Kartik Budania <kartik.budania.02@gmail.com>
2023-05-29 19:08:40 +0530

    home.js

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,48 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Library from '../library/library'
-import Feed from '../feed/feed'
-import Trending from '../trending/trending'
-import Player from '../player/player'
-import Favorites from '../favorites/favorites'
-import './home.css'
-import Sidebar from '../../components/sidebar/sidebar'
-import Login from '../auth/login'
-import { setClientToken } from '../../spotify'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Library from "../library/library";
+import Feed from "../feed/feed";
+import Trending from "../trending/trending";
+import Player from "../player/player";
+import Favorites from "../favorites/favorites";
+import "./home.css";
+import Sidebar from "../../components/sidebar/sidebar";
+import Login from "../auth/login";
+import { setClientToken } from "../../spotify";
 
 export default function Home() {
-  const [token, setToken] = useState("")
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    const token = window.localStorage.getItem('token');
+    const token = window.localStorage.getItem("token");
     const hash = window.location.hash;
     window.location.hash = "";
-    if(!token && hash){
+    if (!token && hash) {
       const _token = hash.split("&")[0].split("=")[1];
-      window.localStorage.setItem('token', _token);
+      window.localStorage.setItem("token", _token);
       setToken(_token);
       setClientToken(_token);
-    }else{
-      setToken(token)
-      setClientToken(token)
+    } else {
+      setToken(token);
+      setClientToken(token);
     }
-  }, [])
+  }, []);
 
   return !token ? (
     <Login />
   ) : (
     <Router>
-      <div className='main-body'>
+      <div className="main-body">
         <Sidebar />
         <Routes>
-          <Route path='/' element={<Library />} />
-          <Route path='/Harmony' element={<Library />} />
-          <Route path='/feed' element={<Feed />} />
-          <Route path='/trending' element={<Trending />} />
-          <Route path='/player' element={<Player />} />
-          <Route path='/favorites' element={<Favorites />} />
+          <Route path="/" element={<Library />} />
+          <Route path="/Harmony" element={<Library />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
