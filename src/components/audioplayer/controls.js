fdaca82a0cd2f2794832927f6ef5192bd520c71f
Kartik Budania <kartik.budania.02@gmail.com>
2023-06-05 22:56:50 +0530

    controls.js

--- src/components/audioplayer/controls.js
+++ src/components/audioplayer/controls.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import './controls.css'
-import { IconContext } from 'react-icons'
-import {FaPause} from 'react-icons/fa'
-import {IoPlaySkipBack, IoPlaySkipForward, IoPlay} from 'react-icons/io5'
+import React from "react";
+import "./controls.css";
+import { IconContext } from "react-icons";
+import { FaPause } from "react-icons/fa";
+import { IoPlaySkipBack, IoPlaySkipForward, IoPlay } from "react-icons/io5";
 
-export default function Controls({isPlaying, setIsPlaying, handleNext, handlePrev}) {
+export default function Controls({
+  isPlaying,
+  setIsPlaying,
+  handleNext,
+  handlePrev,
+}) {
   return (
-    <IconContext.Provider value={{size: '35px', color: '#c4d0e3'}}>
-      <div className='controls-wrapper flex'>
-        <div className='action-btn flex' onClick={handlePrev}>
-        <IoPlaySkipBack />
+    <IconContext.Provider value={{ size: "35px", color: "#c4d0e3" }}>
+      <div className="controls-wrapper flex">
+        <div className="action-btn flex" onClick={handlePrev}>
+          <IoPlaySkipBack />
         </div>
-        <div className={isPlaying? 'play-pause-btn flex active' : 'play-pause-btn flex'} onClick={() => setIsPlaying(!isPlaying)}>
-          {isPlaying? <FaPause /> : <IoPlay />}
+        <div
+          className={
+            isPlaying ? "play-pause-btn flex active" : "play-pause-btn flex"
+          }
+          onClick={() => setIsPlaying(!isPlaying)}
+        >
+          {isPlaying ? <FaPause /> : <IoPlay />}
         </div>
-        <div className='action-btn flex' onClick={handleNext}>
-        <IoPlaySkipForward />
+        <div className="action-btn flex" onClick={handleNext}>
+          <IoPlaySkipForward />
         </div>
       </div>
     </IconContext.Provider>
-  )
+  );
 }
