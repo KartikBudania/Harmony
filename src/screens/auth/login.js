d58da30620cc331928e5c5c6efc197ef5a92263d
Kartik Budania <kartik.budania.02@gmail.com>
2023-06-12 13:39:27 +0530

    added note on the login page

--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -13,6 +13,10 @@ export default function Login() {
       <a href={loginEndpoint}>
         <div className="login-btn">LOG IN</div>
       </a>
+      <div className="note">
+      <p>This project relies on the Spotify API to function properly. It's important to note that Spotify only provides a maximum of 30 seconds of content through their API. This means that any audio or music you encounter on the website will be limited to a 30-second preview. If you wish to access the full content, you may need to consider using the official Spotify application or website directly.</p>
+      <p>It's important to note that Spotify does not always provide an audio source for every song in their vast catalog.Therefore, certain tracks are unavailable for playback due to restrictions imposed by Spotify or the artists themselves. Unfortunately, this is beyond our control as it is determined by the rights holders.</p>
+      </div>
     </div>
   );
 }
